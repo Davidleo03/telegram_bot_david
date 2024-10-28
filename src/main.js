43b81f70ad4d53c4e2fa204bcd9056ae1b31d64b
David López <133236822+Davidleo03@users.noreply.github.com>
2024-10-28 18:31:22 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import bot from './bot.js';
 
 const port = 3000 || process.env.PORT;
 
-createServer(await bot.createWebhook({ domain: "https://telegram-bot-gpt-her2.onrender.com" })).listen(port);
+createServer(await bot.createWebhook({ domain: "https://telegram-bot-david.onrender.com" })).listen(port);
 
 console.log('Bot está funcionando...');
