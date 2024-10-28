ccb7023d7ae27c7afd79f28c35ae6e06dccaf70a
David López <133236822+Davidleo03@users.noreply.github.com>
2024-10-28 18:33:35 -0400

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,6 @@ import bot from './bot.js';
 
 const port = 3000 || process.env.PORT;
 
-createServer(await bot.createWebhook({ domain: "https://telegram-bot-david.onrender.com" })).listen(port);
+createServer(await bot.createWebhook({ domain: "https://telegram-bot-david-u9x7.onrender.com" })).listen(port);
 
 console.log('Bot está funcionando...');
